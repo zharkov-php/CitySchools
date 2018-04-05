47906bcc02b9d073c45d23e95772f9f31975f3b0
Zharkov Andrey <zharkov.php@gmail.com>
2018-04-05 11:52:02 +0300

    Вывод метатегов с админки по школам

--- frontend/modules/kiev/views/avtoshkola/view.php
+++ frontend/modules/kiev/views/avtoshkola/view.php
@@ -5,5 +5,15 @@
  * Date: 04.04.2018
  * Time: 23:34
  */
+$this->title = $kiev_avtoshkola->title_seo;
+//$this->keywords = $kiev_avtoshkola->description_seo;
+\Yii::$app->view->registerMetaTag([
+    'name' => 'description',
+    'content' =>  $kiev_avtoshkola->description_seo
+]);
+\Yii::$app->view->registerMetaTag([
+    'name' => 'keywords',
+    'content' =>  $kiev_avtoshkola->keywords_seo
+]);
 
 echo $kiev_avtoshkola->name;
