f506a0ca2e8ee95b57891d9cc142e5dbea0a0954
Zharkov Andrey <zharkov.php@gmail.com>
2018-04-24 16:59:51 +0300

    Поправил вывод статей

--- frontend/modules/post/controllers/DefaultController.php
+++ frontend/modules/post/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace frontend\modules\post\controllers;
 use Yii;
 use yii\data\Pagination;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 use yii\web\UploadedFile;
 use frontend\modules\post\models\forms\PostForm;
 use frontend\models\Post;
@@ -16,99 +17,19 @@ class DefaultController extends Controller
 
     public function actionIndex()
     {
-        $allPosts = Post::find()->all();
-        $query = Post::find();
-        $countQuery = clone $query;
+        $allPosts = Post::find()->orderBy('id DESC');
+        $countQuery = clone $allPosts;
         $pages = new Pagination(['totalCount' => $countQuery->count(), 'pageSize' => 3]);
         $pages->pageSizeParam = false;
-        $models = $query->offset($pages->offset)
+        $allPosts = $countQuery->offset($pages->offset)
             ->limit($pages->limit)
             ->all();
         return $this->render('index',[
             'allPosts' => $allPosts,
-            'models' => $models,
             'pages' => $pages,
 
         ]);
     }
 
-    /**
-     * Renders the create view for the module
-     * @return string
-     */
-    public function actionCreate()
-    {
-        if (Yii::$app->user->isGuest) {
-            return $this->redirect(['/user/default/login']);
-        }
-        $model = new PostForm(Yii::$app->user->identity);
-        if ($model->load(Yii::$app->request->post())) {
-           // $model->picture = UploadedFile::getInstance($model, 'picture');
-            if ($model->save()) {
-                Yii::$app->session->setFlash('success', 'Post created!');
-                return $this->goHome();
-            }
-        }
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-    /**
-     * Renders the create view for the module
-     * @return string
-     */
-    public function actionView($id)
-    {
-        /* @var $currentUser User */
-        $currentUser = Yii::$app->user->identity;
-        return $this->render('view', [
-            'post' => $this->findPost($id),
-            'currentUser' => $currentUser,
-        ]);
-    }
-    /**
-     * @param integer $id
-     * @return User
-     * @throws NotFoundHttpException
-     */
-    private function findPost($id)
-    {
-        if ($user = Post::findOne($id)) {
-            return $user;
-        }
-        throw new NotFoundHttpException();
-    }
 
-    public function actionLike()
-    {
-        if (Yii::$app->user->isGuest) {
-            return $this->redirect(['/user/default/login']);
-        }
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $id = Yii::$app->request->post('id');
-        $post = $this->findPost($id);
-        /* @var $currentUser User */
-        $currentUser = Yii::$app->user->identity;
-        $post->like($currentUser);
-        return [
-            'success' => true,
-            'likesCount' => $post->countLikes(),
-        ];
-    }
-    public function actionUnlike()
-    {
-        if (Yii::$app->user->isGuest) {
-            return $this->redirect(['/user/default/login']);
-        }
-        Yii::$app->response->format = Response::FORMAT_JSON;
-        $id = Yii::$app->request->post('id');
-        /* @var $currentUser User */
-        $currentUser = Yii::$app->user->identity;
-        $post = $this->findPost($id);
-        $post->unLike($currentUser);
-        return [
-            'success' => true,
-            'likesCount' => $post->countLikes(),
-        ];
-    }
 }
--- frontend/modules/post/views/default/index.php
+++ frontend/modules/post/views/default/index.php
@@ -7,13 +7,7 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Привет!!!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
 
     <div class="body-content">
 
@@ -23,18 +17,18 @@ $this->title = 'My Yii Application';
 
 
 
-              <?php foreach ($models as $model): ?>
+              <?php foreach ($allPosts as $post): ?>
 
 
                 <div class="col-lg-4">
-                    <h2> <?php echo $model->title; ?></h2>
+                    <h2> <?php echo $post->title; ?></h2>
 
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                         dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                         ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                         fugiat nulla pariatur.</p>
 
-                    <p><a class="btn btn-default" href="#"><?php echo Html::encode($model->title); ?>&raquo;</a></p>
+                    <p><a class="btn btn-default" href="<?php echo Url::to(['/post/post/view', 'id' => $post->id]); ?>"><?php echo Html::encode($post->title); ?>&raquo;</a></p>
 
                 </div>
 
